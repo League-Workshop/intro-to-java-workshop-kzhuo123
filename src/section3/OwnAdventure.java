4b4b0bfb50ac486ca34065975736868a16a7919e
RobloxYesCaillouNo <league@WorthyMini-5.attlocal.net>
2018-06-20 11:55:46 -0700

    day 3

--- src/section3/OwnAdventure.java
+++ src/section3/OwnAdventure.java
@@ -9,7 +9,7 @@ public class OwnAdventure {
 	JOptionPane.showMessageDialog(null,"Multiply the new number by 5");
 	JOptionPane.showMessageDialog(null,"Divide the new number by the original number");
 	JOptionPane.showMessageDialog(null,"Subtract 7");
-	JOptionPane.showConfirmDialog("Was it 3?", 1, 0 );
+	JOptionPane.showConfirmDialog(null,"Was it 3?");
 	}
 }
 
